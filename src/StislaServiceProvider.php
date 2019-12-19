41e410544bad70ffcb0b2054e24623f0a4295ed4
M. Rizky Hidayat <rizky@jalameta.co.id>
2019-12-19 17:47:43 +0700

    Fix error stisla submodule not updated!

--- src/StislaServiceProvider.php
+++ src/StislaServiceProvider.php
@@ -15,6 +15,10 @@ class StislaServiceProvider extends ServiceProvider
     public function boot()
     {
         UiCommand::macro('stisla', function (UiCommand $command) {
+            $command->info('Get stisla from stisla/stisla.');
+            exec('git -C vendor/poteto-dev/laravel-ui-stisla submodule init');
+            exec('git -C vendor/poteto-dev/laravel-ui-stisla submodule update');
+
             StislaPreset::install($command);
 
             $command->info('Installing package.');
