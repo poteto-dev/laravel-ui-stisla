d9db81968f5b8e30fe2021a59ac2ba6c52b75d98
tm69 <tuliodiaz@gmail.com>
2020-02-11 12:11:18 -0700

    Update sidebar.blade.php
    fix icon align center when compact navigation

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -7,9 +7,9 @@
   </div>
   <ul class="sidebar-menu">
     <li class="menu-header">Dashboard</li>
-    <li class="{{ request()->is('/') ? 'active' : '' }}"><a class="nav-link" href="{{ url('/') }}"><i class="fa fa-columns"></i> <span>Dashboard</span></a></li>
-    <li class="{{ request()->is('table') ? 'active' : '' }}"><a href="{{ url('table') }}"><i class="fa fa-table"></i> <span>Tables</span></a></li>
+    <li class="{{ request()->is('/') ? 'active' : '' }}"><a class="nav-link" href="{{ url('/') }}"><i class="fas fa-columns"></i> <span>Dashboard</span></a></li>
+    <li class="{{ request()->is('table') ? 'active' : '' }}"><a href="{{ url('table') }}"><i class="fas fa-table"></i> <span>Tables</span></a></li>
     <li class="menu-header">Users</li>
-    <li><a class="nav-link" href=""><i class="fa fa-users"></i> <span>Users</span></a></li>
+    <li><a class="nav-link" href=""><i class="fas fa-users"></i> <span>Users</span></a></li>
   </ul>
 </aside>
